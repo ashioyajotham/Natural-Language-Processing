72d063eec022dcf0b630faee6c110abfe3536671
ashioyajotham <victorashioya960@gmail.com>
2023-06-13 02:21:55 +0300

    gpt-drive

--- LangChain/gpt_drive/gpt_drive.py
+++ LangChain/gpt_drive/gpt_drive.py
@@ -1,9 +1,9 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import GoogleDriveLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI # ChatOpenAI enables you to chat with GPT-3
+from langchain.chains import RetrievalQA # RetrievalQA enables you to retrieve answers from a vector store
+from langchain.document_loaders import GoogleDriveLoader # GoogleDriveLoader enables you to load documents from Google Drive
+from langchain.embeddings import OpenAIEmbeddings # OpenAIEmbeddings enables you to embed text with GPT-3 ie convert text to vectors
+from langchain.vectorstores import Chroma # Chroma enables you to store vectors
+from langchain.text_splitter import RecursiveCharacterTextSplitter # RecursiveCharacterTextSplitter enables you to split text into chunks
 import os
 
 from dotenv import load_dotenv
@@ -27,11 +27,10 @@ texts = text_splitter.split_documents(docs)
 
 #persist_directory = "gpt_drive"
 #metadata = {"folder_id": folder_id}
-db = Chroma.from_documents(texts, embedding=OpenAIEmbeddings(model="davinci"), 
-                           collection_name="apple", metadata=None)
+db = Chroma.from_documents(texts, embedding=OpenAIEmbeddings(model="davinci"), collection_name='annualreports')
 retriever = db.as_retriever()
 
-llm = ChatOpenAI(temperature=0, model_name="davinci")
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
 while True:
@@ -39,5 +38,4 @@ while True:
     answer = qa.run(question)
     print(answer)
 
-# To run the script, type
-# python gpt_drive.py
+#You: What is the revenue of Apple?
