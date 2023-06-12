66773f7529eea83014c3aaaef6a66a391f5e5e51
ashioyajotham <victorashioya960@gmail.com>
2023-06-13 00:41:53 +0300

    langchain

--- LangChain/Readme.md
+++ LangChain/Readme.md
@@ -1,5 +1,6 @@
 ## LangChain
-* LangChain is a framework for building complex LLM powered apps. It was developed by Harrison Chase.
+* LangChain is a framework for building complex LLM powered apps. It is a Python-based framework for chaining together different methods to interact with LLMs. It was developed by Harrison Chase to reduce the complexity of developing applications with LLMs, making it easier for developers to create powerful applications.
+
 * Ways to utilize it:
  - LLM app to take action
  - Bring your own data
--- LangChain/gpt_drive/gpt_drive.py
+++ LangChain/gpt_drive/gpt_drive.py
@@ -4,9 +4,13 @@ from langchain.document_loaders import GoogleDriveLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 folder_id = '1K7qXSMy_SKkug3ZX5DN-2dkkrKPUkPH8'
-loader = GoogleDriveLoader(folder_id, recursive=False)
+loader = GoogleDriveLoader(folder_id = folder_id, 
+                           recursive=False)
 
 docs = loader.load()
 
@@ -14,9 +18,12 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=4000, chunk_overlap=0, separators=["", "\n", ","]
 )
 
-text = text_splitter.split(docs)
-embeddings = OpenAIEmbeddings()
-db = Chroma.from_documents(text, embeddings)
+texts = text_splitter.split_documents(docs)
+embeddings = OpenAIEmbeddings(model="davinci")
+
+#persist_directory = "gpt_drive"
+#metadata = {"folder_id": folder_id}
+db = Chroma.from_documents(texts, embedding=embeddings, collection_name="apple", metadata=None)
 retriever = db.as_retriever()
 
 llm = ChatOpenAI(temperature=0, model_name="davinci")
@@ -26,3 +33,6 @@ while True:
     question = input("> ")
     answer = qa.run(question)
     print(answer)
+
+# To run the script, type
+# python gpt_drive.py
