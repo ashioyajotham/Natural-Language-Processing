a095482aafd62131cd71d7d1d5d7bc6961c8a4e0
ashioyajotham <victorashioya960@gmail.com>
2023-06-12 05:38:47 +0300

    langchain

---
+++ LangChain/Readme.md
@@ -0,0 +1,8 @@
+## LangChain
+* LangChain is a framework for building complex LLM powered apps. It was developed by Harrison Chase.
+* Ways to utilize it:
+ - LLM app to take action
+ - Bring your own data
+ 
+
+[CC](https://haihai/gpt-drive/)
---
+++ LangChain/gpt_drive.py
@@ -0,0 +1,28 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import GoogleDriveLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+folder_id = ''
+loader = GoogleDriveLoader(folder_id, recursive=False)
+
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=4000, chunk_overlap=0, separators=["", "\n", ","]
+)
+
+text = text_splitter.split(docs)
+embeddings = OpenAIEmbeddings()
+db = Chroma.from_documents(text, embeddings)
+retriever = db.as_retriever()
+
+llm = ChatOpenAI(temperature=0, model_name="davinci")
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+while True:
+    question = input("> ")
+    answer = qa.run(question)
+    print(answer)
