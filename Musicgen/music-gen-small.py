f50043ea9105af675c40fd346030d805f5020f7a
Victor Jotham Ashioya <victorashioya960@gmail.com>
2023-06-14 01:47:45 +0300

    Update music-gen-small.py

---
+++ Musicgen/music-gen-small.py
@@ -0,0 +1,17 @@
+pip install git+https://github.com/facebookresearch/audiocraft.git
+
+  import torchaudio
+
+from audiocraft.models import MusicGen
+from audiocraft.data.audio import audio_write
+
+model = MusicGen.get_pretrained('small')
+model.set_generation_params(duration=8)  # generate 8 seconds.
+
+descriptions = ['happy rock', 'energetic EDM', 'sad jazz']
+
+wav = model.generate(descriptions)  # generates 3 samples.
+
+for idx, one_wav in enumerate(wav):
+    # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
+    audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness")
